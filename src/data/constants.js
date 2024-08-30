fe843a4a4ae68ef362467d1729df1aeab4ee4f2a
Abhishek Sharma <abhishek751982@gmail.com>
2024-08-30 19:51:44 +0530

    updated bio

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "Programmer",
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    "I am a motivated and adaptable individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth-oriented mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/abhishek751982",
   resume:
     "https://drive.google.com/file/d/1Bv8-5t9l-JN8-Q4B0l_qZn2RLhZ2ftsx/view?usp=sharing",
